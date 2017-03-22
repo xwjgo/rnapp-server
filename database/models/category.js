b94e64b4abca587fad6643d4b5bcca9503ace48e
xwjgo <wenjiangxu@126.com>
2017-03-22 16:03:01 +0800

    定义mongoose models

---
+++ database/models/category.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const Schema = require('mongoose').Schema;
+const ObjectId = Schema.Types.ObjectId;
+const db = require('../database');
+
+/**
+ * 课程分类存储结构
+ * @property {String} category_name - 分类名称
+ * @property {Array} courses - 该分类下所有课程的ObjectId
+ */
+const categorySchema = new Schema({
+    category_name: {
+        type: String,
+        required: true
+    },
+    courses: [ObjectId]
+});
+
+module.exports = db.model('category', categorySchema);
---
+++ database/models/course.js
@@ -0,0 +1,42 @@
+const Schema = require('mongoose').Schema;
+const ObjectId = Schema.Types.ObjectId;
+const db = require('../database');
+
+/**
+ * 课程内容存储结构
+ * @property {String} course_name - 课程名称
+ * @property {String} teacher - 教师名称
+ * @property {Date} post_time - 课程发布时间
+ * @property {String} picture - 课程封面图片的路径
+ * @property {String} category - 课程所属分类
+ * @property {String} description - 课程描述
+ * @property {Array} chapters - 课程章节
+ * @property {Array} comments - 课程评论id
+ * @property {Array} be_liked - 收藏该课程的用户id
+ */
+const courseSchema = new Schema({
+    course_name: {
+        type: String,
+        required: true
+    },
+    teacher: String,
+    post_time: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    picture: String,
+    category: String,
+    description: String,
+    chapters: [{
+        title: String,
+        sections: [{
+            title: String,
+            content: String
+        }]
+    }],
+    comments: [ObjectId],
+    be_liked: [ObjectId]
+});
+
+module.exports = db.model('course', courseSchema);
