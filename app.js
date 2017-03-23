afce59aab776d6b0f708cee8ee62b7bd005bb1d6
xwjgo <wenjiangxu@126.com>
2017-03-23 20:58:33 +0800

    serve-favicon

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 'use strict';
 
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const favicon = require('serve-favicon');
 const settings = require('./settings');
 const routers = require('./routers');
 const app = express();
 const isProduction = (settings.env === 'production');
+
+// server-favicon
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico'), {
+    maxAge: 2592000000
+}));
+
+// serve静态资源
+app.use(express.static(path.join(__dirname, 'public')));
 
 // 日志打印
 if (!isProduction) {
